9fa24e91220aaf947d58c08779d5a950c29e3c8e
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-01-21 23:59:29 +0530

    Added more TCs for BackspacesInString problem

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/BackspacesInString.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/BackspacesInString.java
@@ -10,8 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BackspacesInString {
     @Test
     public void tests() {
-        assertEquals("ac", cleanString("abc#d##c"));
-        assertEquals("", cleanString("abc####d##c#"));
+      assertEquals("ac", cleanString("abc#d##c"));
+      assertEquals("", cleanString("abc####d##c#"));
+      assertEquals("jf", cleanString("abjd####jfk#"));
+      assertEquals("gdasda", cleanString("gfh#jds###d#dsd####dasdaskhj###dhkjs####df##s##d##"));
+      assertEquals("6+yqw8hfklsd-=-f", cleanString("831####jns###s#cas/*####-5##s##6+yqw87e##hfklsd-=-28##fds##"));
+      assertEquals("jdsfdasns", cleanString("######831###dhkj####jd#dsfsdnjkf###d####dasns"));
+      assertEquals("", cleanString(""));
+      assertEquals("", cleanString("#######"));
+      assertEquals("sa", cleanString("####gfdsgf##hhs#dg####fjhsd###dbs########afns#######sdanfl##db#####s#a"));
+      assertEquals("hskjdgd", cleanString("#hskjdf#gd"));
+      assertEquals("hsk48hjjdfgd", cleanString("hsk48hjjdfgd"));
+      assertEquals("Io4f", cleanString("fjnwui#NI#(*N#ION#Onfjen################Io4f"));
+      assertEquals("6d87hbaskj$$%$$2332ff", cleanString("####6d87hbaskjdnf###$$%W#$@#$2332fr#f"));
+      assertEquals("9OooooO", cleanString("#9#9#9#9o#9#9#9#Oooooo#OOOooO#OO######"));
+      assertEquals("0", cleanString("0###0###0"));
+      assertEquals("904rfDj*fsf09mfednkmfd;m", cleanString("904rfn#jlkcn#####Djva2###*(#fsdgfd####fsdg###09849###mfenf##dnjn##kmfd;l#mg03###"));
     }
 
     //naive
