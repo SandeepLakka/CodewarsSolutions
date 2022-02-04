d5d0172cec0835e0fca3408e6b93e42616d08232
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-02-04 18:54:38 +0530

    updated TransportationOnVacation TCs

--- src/test/java/io/github/sandeeplakka/codewars/kyu8/TransportationOnVacation.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu8/TransportationOnVacation.java
@@ -22,29 +22,32 @@ Write a code that gives out the total amount for different days(d).
 Categories : Fundamentals
  */
 public class TransportationOnVacation {
-  @Test
-  public void under3Tests() {
-    assertEquals(40, rentalCarCost(1));
-    assertEquals(80, rentalCarCost(2));
-  }
+    @Test
+    public void under3Tests() {
+        String msg = "Should work for under 3 days";
+        assertEquals(40, rentalCarCost(1), msg);
+        assertEquals(80, rentalCarCost(2), msg);
+    }
 
-  @Test
-  public void under7Tests() {
-    assertEquals(100, rentalCarCost(3));
-    assertEquals(140, rentalCarCost(4));
-    assertEquals(180, rentalCarCost(5));
-    assertEquals(220, rentalCarCost(6));
-  }
+    @Test
+    public void under7Tests() {
+        String msg = "Should work for under 7 days";
+        assertEquals(100, rentalCarCost(3), msg);
+        assertEquals(140, rentalCarCost(4), msg);
+        assertEquals(180, rentalCarCost(5), msg);
+        assertEquals(220, rentalCarCost(6), msg);
+    }
 
-  @Test
-  public void over7Tests() {
-    assertEquals(230, rentalCarCost(7));
-    assertEquals(270, rentalCarCost(8));
-    assertEquals(310, rentalCarCost(9));
-    assertEquals(350, rentalCarCost(10));
-  }
+    @Test
+    public void over7Tests() {
+        String msg = "Should work for over 7 days";
+        assertEquals(230, rentalCarCost(7), msg);
+        assertEquals(270, rentalCarCost(8), msg);
+        assertEquals(310, rentalCarCost(9), msg);
+        assertEquals(350, rentalCarCost(10), msg);
+    }
 
-  public static int rentalCarCost(int d) {
-    return d * 40 + (d >= 7 ? -50 : (d >= 3 ? -20 : 0));
-  }
+    public static int rentalCarCost(int d) {
+        return d * 40 + (d >= 7 ? -50 : (d >= 3 ? -20 : 0));
+    }
 }
