fb8e27ef218d51b4545919c30324d66e4f846954
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-02-06 22:25:28 +0530

    Added CamelCaseBreaker problem

---
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/CamelCaseBreaker.java
@@ -0,0 +1,30 @@
+package io.github.sandeeplakka.codewars.kyu6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CamelCaseBreaker {
+    @Test
+    public void tests() {
+        assertEquals("camel Casing", camelCase("camelCasing"), "Incorrect");
+        assertEquals("camel Casing Test", camelCase("camelCasingTest"), "Incorrect");
+        assertEquals("camelcasingtest", camelCase("camelcasingtest"), "Incorrect");
+        assertEquals("", camelCase(""), "Incorrect");
+    }
+
+    public static String camelCase(String input) {
+        if (input == null || input.length() < 2) return input;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char curChar = input.charAt(i);
+            if (Character.isUpperCase(curChar)) {
+                builder.append(" ").append(curChar);
+            } else {
+                builder.append(curChar);
+            }
+        }
+        return builder.toString();
+    }
+
+}
