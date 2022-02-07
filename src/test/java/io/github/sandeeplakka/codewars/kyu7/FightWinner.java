4b292eaa6949ee9bd854ade0102fff738198ad24
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-02-07 21:38:12 +0530

    Updated FightWinner with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/FightWinner.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/FightWinner.java
@@ -4,6 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+Two fighters, one winner.
+
+Description:
+
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills
+the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python)
+will be integers larger than 0. You can mutate the Fighter objects.
+Example:
+
+public class Fighter {
+  public String name;
+  public int health, damagePerAttack;
+  public Fighter(String name, int health, int damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+  }
+}
+
+Categories : Fundamentals, Games, Algorithms, Classes, Basic Language Features, Object-oriented Programming
+ */
 public class FightWinner {
     @Test
     public void basicTests() {
