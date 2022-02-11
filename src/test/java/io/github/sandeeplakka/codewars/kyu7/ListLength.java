85569ee0f485bdc5a2615e898aee01fe39167774
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-02-12 02:39:06 +0530

    Added ListLength problem with iterative solution

---
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/ListLength.java
@@ -0,0 +1,88 @@
+package io.github.sandeeplakka.codewars.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static io.github.sandeeplakka.codewars.kyu7.Helpers.listFromArray;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+Fun with lists: length
+
+Description:
+Implement the method length, which accepts a linked list (head),
+and returns the length of the list.
+For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+
+The linked list is defined as follows:
+
+class Node {
+  public Object data;
+  public Node next;
+
+  Node(T data, Node next) {
+    this.data = data;
+    this.next = next;
+  }
+
+  Node(T data) {
+    this(data, null);
+  }
+}
+
+Note: the list may be null and can hold any type of value.
+Good luck!
+
+Categories : Fundamentals, Lists, Data Structures
+ */
+public class ListLength {
+    @Test
+    public void basicTests() {
+        assertEquals(0, lengthIterative(null));
+        assertEquals(3, lengthIterative(listFromArray(1, 2, 3)));
+        assertEquals(3, lengthIterative(listFromArray("aaa", "b", "abc")));
+        assertEquals(4, lengthIterative(listFromArray(1, 2, 3, 4)));
+    }
+
+    static <T> int lengthIterative(Node head) {
+        if (head == null) return 0;
+        int length = 1;
+        while (head.next != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+}
+
+
+//helper and support code
+class Helpers {
+    public static <T> Node<T> listFromArray(T... arr) {
+        Node<T> head = new Node<>(arr[0]);
+        Node<T> pointer = head;
+        for (int i = 1; i < arr.length; i++) {
+            T val = arr[i];
+            Node<T> curNode = new Node<>(val);
+            Node<T> last = pointer;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = curNode;
+        }
+        return pointer;
+    }
+}
+
+class Node<T> {
+    public T data;
+    public Node next;
+
+    Node(T data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    Node(T data) {
+        this(data, null);
+    }
+}
