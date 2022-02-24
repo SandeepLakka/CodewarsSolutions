7ec37dcbede7b2c4614dafd1113f53588b7a31fd
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-02-24 22:48:03 +0530

    Updated DeadFish with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/DeadFish.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/DeadFish.java
@@ -7,6 +7,24 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+/*
+Make the Deadfish Swim
+
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+    i increments the value (initially 0)
+    d decrements the value
+    s squares the value
+    o outputs the value into the return array
+
+Invalid characters should be ignored.
+
+Deadfish.parse("iiisdoso") =- new int[] {8, 64};
+
+Categories : Algorithms, Parsing, Strings
+ */
 public class DeadFish {
 
     @Test
