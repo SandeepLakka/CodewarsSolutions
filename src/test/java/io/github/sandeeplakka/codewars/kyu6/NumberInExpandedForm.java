f6428f466884b6cd2d0690e91f1d4554ba44bcb2
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-02-25 21:46:20 +0530

    Updated NumberInExpandedForm with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/NumberInExpandedForm.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/NumberInExpandedForm.java
@@ -9,6 +9,20 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+Write Number in Expanded Form
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+Kata.expandedForm(12); # Should return "10 + 2"
+Kata.expandedForm(42); # Should return "40 + 2"
+Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
+
+NOTE: All numbers will be whole numbers greater than 0.
+
+If you liked this kata, check out part 2!!
+Categories : Fundamentals, Strings, Numbers, Mathematics, Algorithms
+ */
 public class NumberInExpandedForm {
     @Test
     public void test() {
