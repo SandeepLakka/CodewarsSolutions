7f210a8dc7d7066a878d76eb883c841abfbbf9e6
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-03-01 00:10:35 +0530

    Updated PinValidator problem with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/PinValidator.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/PinValidator.java
@@ -5,6 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/*
+Regex validate PIN code
+
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+Examples (Input --> Output)
+
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+
+Categories : Fundamentals, Regular Expressions, Declarative Programming
+Advanced Language Features, Strings
+ */
 public class PinValidator {
 
     @Test
