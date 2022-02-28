6d25cc2a04820e3a245a66321f997c784e86ced0
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-03-01 00:14:40 +0530

    Updated DoubleChars problem with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu8/DoubleChars.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu8/DoubleChars.java
@@ -11,6 +11,20 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+Double Char
+
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+doubleChar("1234!_ ") ==> "11223344!!__  "
+
+Good Luck!
+
+Categories :  Fundamentals, Loops, Control Flow, Basic Language Features
+
+ */
 public class DoubleChars {
     @Test
     void fixedTests1_for_doubleChar() {
