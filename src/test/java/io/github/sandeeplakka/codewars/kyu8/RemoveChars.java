1f8d06b29b49fac082d351ffcfbb10a8622088a2
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-03-01 00:20:13 +0530

    Updated StringToArray problem with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu8/RemoveChars.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu8/RemoveChars.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
-It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+Remove First and Last Character
+
+It's pretty straightforward.
+Your goal is to create a function that removes the first and last characters of a string.
 You're given one parameter, the original string.
 You don't have to worry with strings with less than two characters.
 
+Categories : Fundamentals, Basic Language Features, Strings
 
  */
 public class RemoveChars {
--- src/test/java/io/github/sandeeplakka/codewars/kyu8/StringToArray.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu8/StringToArray.java
@@ -4,6 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+/*
+Convert a string to an array
+
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+Categories : Fundamentals, Arrays, Strings
+ */
 public class StringToArray {
     @Test
     public void basicTests() {
