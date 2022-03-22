23d3d765831ccc7f95b47eb5e2feefe36ff9f79a
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-03-22 20:12:57 +0530

    Added more TCs to CountIPAddresses problem

--- src/test/java/io/github/sandeeplakka/codewars/kyu5/CountIPAddresses.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu5/CountIPAddresses.java
@@ -25,10 +25,19 @@ Categories : Algorithms, Parsing, Strings
 public class CountIPAddresses {
 
     @Test
-    public void test1() {
+    public void fixedTests() {
         assertEquals(50, ipsBetween("10.0.0.0", "10.0.0.50"));
         assertEquals(246, ipsBetween("20.0.0.10", "20.0.1.0"));
-        assertEquals(256, ipsBetween("10.0.0.0", "10.0.1.0"));
+        assertEquals((1L << 32L) - 1L, ipsBetween("0.0.0.0", "255.255.255.255"));
+        assertEquals(1, ipsBetween("150.0.0.0", "150.0.0.1"));
+        assertEquals(50, ipsBetween("10.0.0.0", "10.0.0.50"));
+        assertEquals(246, ipsBetween("20.0.0.10", "20.0.1.0"));
+        assertEquals(243, ipsBetween("10.11.12.13", "10.11.13.0"));
+        assertEquals(256, ipsBetween("160.0.0.0", "160.0.1.0"));
+        assertEquals(65536, ipsBetween("170.0.0.0", "170.1.0.0"));
+        assertEquals(65793, ipsBetween("50.0.0.0", "50.1.1.1"));
+        assertEquals(16777216, ipsBetween("180.0.0.0", "181.0.0.0"));
+        assertEquals(67372036, ipsBetween("1.2.3.4", "5.6.7.8"));
     }
 
     private static int[] getOctets(String ipAddress) {
