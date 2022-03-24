be0126140de4a22a040fb74d137aeeb4f76a7faa
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-03-25 00:48:56 +0530

    Updated finished version of Equation problem with TCs

--- src/test/java/io/github/sandeeplakka/codewars/kyu4/Equation.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu4/Equation.java
@@ -25,97 +25,59 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Equation {
 
     @Test
-    public void sampleTests() {
+    public void sampleTestsV1() {
         assertEquals(new BigInteger("12"), differentiate("12x+2", 3));
         assertEquals(new BigInteger("5"), differentiate("x^2-x", 3));
         assertEquals(new BigInteger("-20"), differentiate("-5x^2+10x+4", 3));
     }
 
     @Test
-    public void test() {
-        differentiate("x^2-x", 4);
-        differentiate("-5x^2+12x-2", 3);
-    }
-
-    @Test
-    public void te() {
+    public void sampleTestV0() {
         List<String> list = Arrays.asList(
                 "-x", "2x", "-3x^2", "5x^3", "+2x^4", "+4", "-5"
+                //dx :   -1 ,  2  ,  -6x   ,  15x^2,  8x^3  ,  0  ,   0
+                //val:   -1 ,  2  ,  -18   ,  135  ,   216  ,  0  ,   0
         );
-        list.stream().map(Term::new).map(Term::differentiatedTerm).forEach(System.out::println);
+        BigInteger[] expectedVals = new BigInteger[]{
+                BigInteger.valueOf(-1), BigInteger.valueOf(2), BigInteger.valueOf(-18),
+                BigInteger.valueOf(135), BigInteger.valueOf(216), BigInteger.valueOf(0),
+                BigInteger.valueOf(0)
+        };
+        assertArrayEquals(expectedVals, list.stream()
+                .map(Term::new)
+                .map(Term::differentiatedTerm)
+                .map(term -> term.returnValue(3)).toArray(BigInteger[]::new));
 
     }
 
     public static BigInteger differentiate(String equation, long x) {
-        List<Integer> coefficients = new ArrayList<>();
-        List<Integer> powers = new ArrayList<>();
-        int i = 0;
-        StringBuilder result = new StringBuilder();
-        String term = "";
-        while (i < equation.length()) {
-            //
-            if (equation.charAt(i) == '+' || equation.charAt(i) == '-') {
-                //System.out.println("Term we have is : "+term);
-                result.append(getCoefficient(term) >= 0 ? "+" : "");
-                result.append(differentiate(term));
-                term = "";
+        List<String> terms = new ArrayList<>();
+        StringBuilder term = new StringBuilder();
+        for (int i = 0; i < equation.length(); i++) {
+            if ((equation.charAt(i) == '+' || equation.charAt(i) == '-') && i != 0) {
+                terms.add(term.toString());
+                term = new StringBuilder();
             }
-            term += equation.charAt(i);
-            System.out.println("term is : " + term + "\t\tresult is : " + result);
-            i++;
-        }
-        System.out.println("Result : " + result);
-        System.out.println("--------------------");
-        // Your code here!
-        return null;
-    }
-
-    @Test
-    public void test_differentiate() {
-        System.out.println(differentiate("2x") + " == 2");
-        System.out.println(differentiate("-2x^2") + " == -4x");
-        System.out.println(differentiate("100") + " == 0");
-        System.out.println(differentiate("5x^2") + "== +10x");
-        System.out.println(differentiate("-x") + " == -1");
-    }
-
-    private static String differentiate(String term) {
-        if (term.isEmpty()) return "";
-        //x^n = nx^n-1
-        //c = 0
-        //cx = c
-        int coefficient = getCoefficient(term);
-        int power = getPower(term);
-        if (power == 0) return String.valueOf(0);
-        if (power == 1) return String.valueOf(coefficient);
-        return (coefficient * power) + "x" + (power > 2 ? power - 1 : "");
-    }
-
-    private static int getCoefficient(String term) {
-        if (!term.contains("x")) {
-            term = term.substring(0, term.indexOf("x"));
-            if (!term.matches("[+-]?[0-9]+")) {
-                return Integer.parseInt(term + 1);
+            term.append(equation.charAt(i));
+            if (i == equation.length() - 1) {
+                terms.add(term.toString());
             }
-            return Integer.parseInt(term);
         }
-        return Integer.parseInt(term);
-    }
-
-    private static int getPower(String term) {
-        if (!term.contains("x")) return 0;
-        if (!term.contains("^")) return 1;
-        return Integer.parseInt(term.substring(term.indexOf("^") + 1));
+        return terms.stream().map(Term::new)
+                .map(Term::differentiatedTerm)
+                .map(differentiatedTerm -> differentiatedTerm.returnValue(x))
+                .reduce(BigInteger.ZERO, (result, value) -> result = result.add(value));
     }
 }
 
 class Term {
-    final String REGEX = "([-+]?)([\\d]?)(x)?([\\^][\\d])?";
+    final String REGEX = "([-+]?)([\\d]*)(x)?([\\^][\\d]*)?";
     final Pattern pattern = Pattern.compile(REGEX);
 
     private boolean isPositive;
@@ -185,6 +147,23 @@ class Term {
                 ", variable='" + variable + '\'' +
                 ", power=" + power +
                 '}';
+    }
+
+
+    public BigInteger returnValue(long value) {
+        //constants
+        if (coefficient == 0) return BigInteger.ZERO;
+
+        BigInteger result = BigInteger.valueOf(coefficient);
+
+        if (this.power > 0) {
+            BigInteger poweredValue = BigInteger.valueOf(value).pow(power);
+            result = result.multiply(poweredValue);
+        }
+        if (!this.isPositive) {
+            result = result.negate();
+        }
+        return result;
     }
 
     public Term differentiatedTerm() {
