786b03f84bd9b183fa19f43dc1c90b88bdb66f8a
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-04-08 23:47:54 +0530

    Updated HowManyPagesInABook with description

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/HowManyPagesInABook.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/HowManyPagesInABook.java
@@ -8,6 +8,25 @@ import java.util.concurrent.ThreadLocalRandom;
 import static java.lang.Math.log10;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+How many pages in a book?
+
+Description:
+
+Every book has n pages with page numbers 1 to n.
+The summary is made by adding up the number of digits of all page numbers.
+
+Task: Given the summary, find the number of pages n the book has.
+Example
+If the input is summary=25, then the output must be n=17:
+The numbers 1 to 17 have 25 digits in total: 1234567891011121314151617.
+
+Be aware that you'll get enormous books having up to 100.000 pages.
+All inputs will be valid.
+
+
+Categories : Algorithms, Puzzles, Games, Interview Questions
+ */
 public class HowManyPagesInABook {
 
     @Test
