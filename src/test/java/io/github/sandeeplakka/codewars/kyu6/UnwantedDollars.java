7e969ab3202c53891585a12b25e867cc1465f4f0
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-04-12 00:09:41 +0530

    Updated UnwantedDollars problem with different solution impl

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/UnwantedDollars.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/UnwantedDollars.java
@@ -19,6 +19,17 @@ public class UnwantedDollars {
         assertEquals(-0.34, moneyValue("-.34"), 1e-9);
         assertEquals(0.0, moneyValue("$$$"), 1e-9);
 
+        assertEquals(12.34, moneyValueV1("12.34"), 1e-9);
+        assertEquals(5.67, moneyValueV1(" $5.67"), 1e-9);
+        assertEquals(-0.89, moneyValueV1("-0.89"), 1e-9);
+        assertEquals(-0.10, moneyValueV1("-$ 0.1"), 1e-9);
+        assertEquals(-2.3456, moneyValueV1("$-2.3456"), 1e-9);
+        assertEquals(7, moneyValueV1("007"), 1e-9);
+        assertEquals(89.0, moneyValueV1(" $ 89"), 1e-9);
+        assertEquals(0.11, moneyValueV1("   .11"), 1e-9);
+        assertEquals(0.20, moneyValueV1("$.2"), 1e-9);
+        assertEquals(-0.34, moneyValueV1("-.34"), 1e-9);
+        assertEquals(0.0, moneyValueV1("$$$"), 1e-9);
     }
 
     public static double moneyValue(String money) {
@@ -28,5 +39,13 @@ public class UnwantedDollars {
         }
         if (money.trim().length() == 0 || money.trim().equals("-")) return 0.0D;
         return Double.parseDouble(money);
+    }
+
+    public static double moneyValueV1(String money) {
+        try {
+            return Double.parseDouble(money.replaceAll("[ $]", ""));
+        } catch (NumberFormatException e) {
+            return 0.0;
+        }
     }
 }
