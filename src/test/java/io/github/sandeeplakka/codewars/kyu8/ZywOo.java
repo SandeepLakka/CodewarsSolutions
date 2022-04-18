c133131c084a62223c7227c05edb5bab9ae72199
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-04-18 21:06:04 +0530

    Updated ZywOo with more TCs

--- src/test/java/io/github/sandeeplakka/codewars/kyu8/ZywOo.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu8/ZywOo.java
@@ -2,6 +2,8 @@ package io.github.sandeeplakka.codewars.kyu8;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -24,6 +26,35 @@ public class ZywOo {
         assertArrayEquals(new int[]{0, 1, 2},
                 take(new int[]{0, 1, 2, 3, 5, 8, 13}, 3),
                 "should return the first 3 items");
+        assertArrayEquals(new int[0],
+                take(new int[]{0, 1, 2, 3, 5, 8, 13}, 0),
+                "should return 0 items");
+        assertArrayEquals(new int[0],
+                take(new int[0], 3), "empty array should return empty array");
+    }
+
+    @Test
+    public void randomTests() {
+        for (int i = 0; i < 100; i++) {
+            int n = (int) Math.floor(100 * Math.random());
+            int[] randomArray = generateRandomArray();
+
+            assertArrayEquals(solution(randomArray, n),
+                    take(randomArray, n),
+                    "Testing with array " + Arrays.toString(randomArray) + " and n = " + n);
+        }
+
+    }
+
+    private int[] solution(int[] arr, int n) {
+        return arr.length > 0 && arr.length > n ? Arrays.copyOfRange(arr, 0, n) : arr;
+    }
+
+    private int[] generateRandomArray() {
+        int length = (int) Math.floor(100 * Math.random());
+        int min = (int) Math.floor(-100 * Math.random());
+        int max = (int) Math.floor(100 * Math.random());
+        return new Random().ints(length, min, max).toArray();
     }
 
     public static int[] take(int[] arr, int n) {
