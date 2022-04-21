84cbc4d3988471a15a8e015583d2c45950fd4f65
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-04-21 08:56:42 +0530

    Updated Palindromes to add dp approach impl

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/Palindromes.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/Palindromes.java
@@ -35,6 +35,17 @@ public class Palindromes {
     }
 
     @Test
+    public void basicTests_for_V1_impl() {
+        assertEquals(1, longestPalindromeV1("a"));
+        assertEquals(2, longestPalindromeV1("aa"));
+        assertEquals(2, longestPalindromeV1("baa"));
+        assertEquals(2, longestPalindromeV1("aab"));
+        assertEquals(1, longestPalindromeV1("zyabyz"));
+        assertEquals(4, longestPalindromeV1("baabcd"));
+        assertEquals(9, longestPalindromeV1("baablkj12345432133d"));
+    }
+
+    @Test
     public void twistedTests() {
         assertEquals(63,
                 longestPalindrome("!loj!bu 7u y8z6rh.pqxwc3aka2r?t. .t?r2aka3cwxqp.hr6z8y u7 ub!jolnwc?jui tpt"));
@@ -56,6 +67,29 @@ public class Palindromes {
                 longestPalindrome("pa8024r8jn49nv2?563i365?2vn94nj8r420qc"));
     }
 
+    @Test
+    public void twistedTests_for_V1_impl() {
+        assertEquals(63,
+                longestPalindromeV1("!loj!bu 7u y8z6rh.pqxwc3aka2r?t. .t?r2aka3cwxqp.hr6z8y u7 ub!jolnwc?jui tpt"));
+        assertEquals(34,
+                longestPalindromeV1("ieic!e9ovmgf ld ee dl fgmvo9e!ciei6hx4vqozz!h zo0u7"));
+        assertEquals(54,
+                longestPalindromeV1("9eu!mw fx5jzn7v9yr  e469fx!?11?!xf964e  ry9v7nzj5xf wm!u4x"));
+        assertEquals(68,
+                longestPalindromeV1("?ihuw7bl?fn2t4 cvr5 oo9rv  zl.vct r sv6yuyjuk5m5  " +
+                        "5m5kujyuy6vs r tcv.lz  vr9oo 5rvci?b cs rcet!"));
+        assertEquals(46,
+                longestPalindromeV1("pe1ftuq?xt ur? m1hml0eppe0lmh1m ?ru tx?qutf1ep"));
+        assertEquals(51,
+                longestPalindromeV1("wyz0z jj8mswgu e0q8 f29o p!2glzs gc.q6f2nkukn2f6q.cg szlg2!p o92f 8q!tp"));
+        assertEquals(91,
+                longestPalindromeV1("e 0zwm3 adng8i?v898?zl5hci00hc842u0qpuq5s 8.zq42b1v0v1b24qz.8 " +
+                        "s5qupq0u248ch00ich5lz?898v?i8gnda 32"));
+        assertEquals(33,
+                longestPalindromeV1("pa8024r8jn49nv2?563i365?2vn94nj8r420qc"));
+    }
+
+    //naive/bruteforce approach
     public static int longestPalindrome(final String s) {
         int highest = -1;
         //if(s.length() <= 1 ) return 1;
@@ -63,19 +97,37 @@ public class Palindromes {
             for (int j = i + 1; j <= s.length(); j++) {
                 String str = s.substring(i, j);
                 String revStr = new StringBuilder(str).reverse().toString();
-                //could've tried linear time complexity palindrome check, but going with these for now
                 if (str.equals(revStr)) {
                     int length = j - i;
-                    if (highest < length) {
-                        highest = length;
-                    }
+                    highest = Math.max(highest, length);
                 }
             }
         }
-        return highest; //your code here
+        return highest;
     }
 
-    public static int longestPalRec(final String s, int l) {
-        return -1;
+
+    public static int longestPalindromeV1(final String s) {
+        boolean[][] memo = new boolean[s.length()][s.length()];
+        int max = 1;
+        for (int i = 0; i < s.length(); i++) {
+            memo[i][i] = true;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                memo[i][i + 1] = true;
+                max = 2;
+            }
+        }
+        for (int len = 3; len <= s.length(); len++) {
+            for (int i = 0; i < s.length() - len + 1; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && memo[i + 1][j - 1]) {
+                    memo[i][j] = true;
+                    max = Math.max(max, len);
+                }
+            }
+        }
+        return max;
     }
 }
