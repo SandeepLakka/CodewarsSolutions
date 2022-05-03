c01532b6796e689bb63ec0354377297104ed9f98
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-05-03 20:02:23 +0530

    Updated problem description for ListLastIndexOfItem problem

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/ListLastIndexOfItem.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/ListLastIndexOfItem.java
@@ -4,6 +4,36 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+Fun with lists: lastIndexOf
+
+Implement the method lastIndexOf (last_index_of in PHP),
+which accepts a linked list (head) and a value, and returns
+the index (zero based) of the last occurrence of that value if exists, or -1 otherwise.
+
+For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, lastIndexOf / last_index_of should return 3.
+
+The linked list is defined as follows:
+
+    class Node {
+      public Object data;
+      public Node next;
+
+      Node(T data, Node next) {
+        this.data = data;
+        this.next = next;
+      }
+
+      Node(T data) {
+        this(data, null);
+      }
+    }
+
+Note: the list may be null and can hold any type of value.
+Good luck!
+
+Categories : Fundamentals, Lists, Data Structures
+ */
 public class ListLastIndexOfItem {
 
     @Test
