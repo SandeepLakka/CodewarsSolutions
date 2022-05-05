6b028184c554efd9ea8cd18ed493b9370bb3bcb7
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-05-05 23:13:48 +0530

    Updated more TCs to ProdFib problem

--- src/test/java/io/github/sandeeplakka/codewars/kyu5/ProdFib.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu5/ProdFib.java
@@ -47,6 +47,78 @@ public class ProdFib {
     }
 
     @Test
+    public void test1() {
+        long[] r = new long[]{55, 89, 1};
+        assertArrayEquals(r, productFib(4895));
+    }
+
+    @Test
+    public void test2() {
+        long[] r = new long[]{89, 144, 0};
+        assertArrayEquals(r, productFib(5895));
+    }
+
+    @Test
+    public void test3() {
+        long[] r = new long[]{6765, 10946, 1};
+        assertArrayEquals(r, productFib(74049690));
+    }
+
+    @Test
+    public void test4() {
+        long[] r = new long[]{10946, 17711, 0};
+        assertArrayEquals(r, productFib(84049690));
+    }
+
+    @Test
+    public void test5() {
+        long[] r = new long[]{10946, 17711, 1};
+        assertArrayEquals(r, productFib(193864606));
+    }
+
+    @Test
+    public void test6() {
+        long[] r = new long[]{610, 987, 0};
+        assertArrayEquals(r, productFib(447577));
+    }
+
+    @Test
+    public void test7() {
+        long[] r = new long[]{610, 987, 1};
+        assertArrayEquals(r, productFib(602070));
+    }
+
+    @Test
+    public void test8() {
+        long[] r = new long[]{832040, 1346269, 0};
+        assertArrayEquals(r, productFib(602070602070L));
+    }
+
+    @Test
+    public void test9() {
+        long[] r = new long[]{832040, 1346269, 1};
+        assertArrayEquals(r, productFib(1120149658760L));
+    }
+
+    @Test
+    public void test10() {
+        long[] r = new long[]{1346269, 2178309, 1};
+        assertArrayEquals(r, productFib(2932589879121L));
+    }
+
+    @Test
+    public void test11() {
+        long[] r = new long[]{1, 1, 1};
+        assertArrayEquals(r, productFib(1));
+    }
+
+    @Test
+    public void test12() {
+        long[] r = new long[]{1836311903L, 2971215073L, 0};
+        assertArrayEquals(r, productFib(4456077604922913920L));
+    }
+
+    @Test
     public void problemStatementGivenTests() {
         long[] result = new long[]{21, 34, 1};
         assertArrayEquals(result, productFib(714));
