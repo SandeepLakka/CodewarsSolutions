b728e44c4742185844c96a81f254a183acab340f
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-05-06 17:48:53 +0530

    Refactor to enhance utility methods to Helpers

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/Helpers.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/Helpers.java
@@ -1,7 +1,13 @@
 package io.github.sandeeplakka.codewars.kyu7;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 //helper and support code
 public class Helpers {
+    @SafeVarargs
     public static <T> Node<T> listFromArray(T... arr) {
         Node<T> pointer = new Node<>(arr[0]);
         for (int i = 1; i < arr.length; i++) {
@@ -14,5 +20,15 @@ public class Helpers {
             last.next = curNode;
         }
         return pointer;
+    }
+
+    public static <T> void testFilter(Node<T> output, Node<T> expected) {
+        assertArrayEquals(listToArray(expected), listToArray(output));
+    }
+
+    private static <T> Object[] listToArray(Node<T> head) {
+        Collection<T> values = new LinkedList<>();
+        for (; head != null; head = head.next) values.add(head.data);
+        return values.toArray();
     }
 }
