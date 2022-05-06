aabebce21bd9dd5d49da733cd7a7abf975b6494d
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-05-06 18:41:58 +0530

    Updated ListMap problem with more TCs

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/ListMap.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/ListMap.java
@@ -3,7 +3,6 @@ package io.github.sandeeplakka.codewars.kyu6;
 import io.github.sandeeplakka.codewars.kyu7.Node;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.function.Function;
@@ -51,18 +50,37 @@ class ListMap {
     public void basicTests() {
         testMap(null, __ -> false, null);
 
+        testMap(new Node<>(1, new Node<>(2, new Node<>(3))),
+                x -> x,
+                new Node<>(1, new Node<>(2, new Node<>(3))));
+
         Function<Integer, Integer> f = x -> x * 2;
         testMap(new Node<>(1, new Node<>(2, new Node<>(3))),
                 f,
                 new Node<>(2, new Node<>(4, new Node<>(6))));
     }
 
-    private static <T, R> void testMap(Node<T> head, Function<T, R> f, Node<R> expected) {
-        assertArrayEquals(listToArray(expected), listToArray(map(head, f)));
+    @Test
+    public void moreTests() {
+        Function<Integer, Integer> f1 = x -> x + 3;
+        testMap(new Node<>(1, new Node<>(2, new Node<>(3))),
+                f1,
+                new Node<>(4, new Node<>(5, new Node<>(6))));
+
+        Function<Integer, Boolean> f2 = x -> x > 1;
+        testMap(new Node<>(1, new Node<>(2, new Node<>(3))),
+                f2,
+                new Node<>(false, new Node<>(true, new Node<>(true))));
+
+        Function<String, String> f3 = x -> x + "x";
+        testMap(new Node<>("aaa", new Node<>("b", new Node<>("abc"))),
+                f3,
+                new Node<>("aaax", new Node<>("bx", new Node<>("abcx"))));
     }
 
-    private static Node<Integer> arrayToList(int[] array) {
-        return array.length == 0 ? null : new Node<>(array[0], arrayToList(Arrays.copyOfRange(array, 1, array.length)));
+
+    private static <T, R> void testMap(Node<T> head, Function<T, R> f, Node<R> expected) {
+        assertArrayEquals(listToArray(expected), listToArray(map(head, f)));
     }
 
     private static <T> Object[] listToArray(Node<T> head) {
