c522f87b9d3ab1cb6160015ada8cbf5967392abd
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-05-06 17:49:38 +0530

    Refactor to correct constructor visibility of Node class

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/Node.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/Node.java
@@ -4,12 +4,12 @@ public class Node<T> {
     public T data;
     public Node<T> next;
 
-    Node(T data, Node<T> next) {
+    public Node(T data, Node<T> next) {
         this.data = data;
         this.next = next;
     }
 
-    Node(T data) {
+    public Node(T data) {
         this(data, null);
     }
 }
