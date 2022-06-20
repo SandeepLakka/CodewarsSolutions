0c52dbc2181248d7b8cc20c27ee992b011735598
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-06-20 22:01:48 +0530

    Added more TCs to ExclamationRemover problem

--- src/test/java/io/github/sandeeplakka/codewars/kyu8/ExclamationRemover.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu8/ExclamationRemover.java
@@ -2,6 +2,10 @@ package io.github.sandeeplakka.codewars.kyu8;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExclamationRemover {
@@ -9,6 +13,37 @@ class ExclamationRemover {
     @Test
     public void testSimpleString1() {
         assertEquals("Hello World", removeExclamationMarks("Hello World!"));
+    }
+
+    @Test
+    public void testSimpleString2() {
+        assertEquals("Hello World", removeExclamationMarks("Hello World!!!"));
+    }
+
+    @Test
+    public void testSimpleString3() {
+        assertEquals("Hi Hello", removeExclamationMarks("Hi! Hello!"));
+    }
+
+    @Test
+    public void testRandomString() {
+        String rand1, rand2, rand3, rand4;
+        rand1 = randomString();
+        rand2 = randomString();
+        rand3 = randomString();
+        rand4 = randomString();
+        String expected = String.format("%s%s %s%s", rand1, rand2, rand3, rand4);
+        String rs = String.format("%s!%s %s!%s", rand1, rand2, rand3, rand4);
+        assertEquals(expected, removeExclamationMarks(rs));
+    }
+
+    private String randomString() {
+        Random random = new Random();
+        String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz";
+        return IntStream.range(0, 10)
+                .mapToObj(__ -> abc.charAt(random.nextInt(abc.length())))
+                .map(String::valueOf)
+                .collect(Collectors.joining(""));
     }
 
     static String removeExclamationMarks(String s) {
