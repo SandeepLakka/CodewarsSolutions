c8fd7488893b074770712edeea10be5d3adc75f6
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-07-24 12:47:35 +0530

    Updated to add description for LargestSumPairInArray problem

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/LargestSumPairInArray.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/LargestSumPairInArray.java
@@ -6,6 +6,20 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+Largest pair sum in array
+
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example:
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+Input sequence contains minimum two elements and every element is an integer.
+
+Categories : Fundamentals
+
+ */
 public class LargestSumPairInArray {
     @Test
     public void basicTestCases() {
