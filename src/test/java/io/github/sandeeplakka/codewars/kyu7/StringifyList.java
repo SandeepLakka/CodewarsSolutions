8f547ccb0106f28dd73b8b9732abaa1391f125f8
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-07-28 09:44:18 +0530

    Updated StringifyList to add description

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/StringifyList.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/StringifyList.java
@@ -7,6 +7,31 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+Convert a linked list to a string
+
+Task
+
+Create a function stringify which accepts an argument list and returns a string representation of the list. 
+The string representation of the list starts with the value of the current Node, specified by its data property, 
+followed by a whitespace character, an arrow and another whitespace character (" -> "), followed by the rest of the list. 
+The end of the string representation of a list must always end with null. 
+
+For example, given the following list:
+
+`new Node(1, new Node(2, new Node(3)))`
+
+its string representation would be: `"1 -> 2 -> 3 -> null"`
+
+Note that null itself is also considered a valid linked list. 
+In that case, its string representation would simply be "null".
+
+For the simplicity of this Kata, you may assume that any Node in this Kata may only contain non-negative integer values. 
+For example, you will not encounter a Node whose data is "Hello World".
+
+
+Categories: Linked Lists, Recursion, Algorithms
+*/
 public class StringifyList {
 
     @Test
