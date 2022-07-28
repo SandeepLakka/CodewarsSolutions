981f23b5727856eb71f354c281b70bc0a33570f4
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-07-28 09:46:35 +0530

    Updated StringifyList to add more TCs

--- src/test/java/io/github/sandeeplakka/codewars/kyu7/StringifyList.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu7/StringifyList.java
@@ -41,6 +41,15 @@ public class StringifyList {
         assertEquals("null", stringify(null));
     }
 
+    @Test
+    public void fixedTest() {
+        assertEquals("23 -> null", stringify(new Node(23)));
+        assertEquals("4 -> 25 -> null", stringify(new Node(4, new Node(25))));
+        assertEquals("1 -> 3 -> 6 -> 10 -> 15 -> 21 -> 28 -> 36 -> 45 -> 55 -> null", stringify(new Node(1, new Node(3, new Node(6, new Node(10, new Node(15, new Node(21, new Node(28, new Node(36, new Node(45, new Node(55))))))))))));
+        assertEquals("1 -> 1 -> 2 -> 3 -> 5 -> 8 -> 13 -> null", stringify(new Node(1, new Node(1, new Node(2, new Node(3, new Node(5, new Node(8, new Node(13)))))))));
+        assertEquals("0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8 -> 13 -> 21 -> 34 -> 55 -> 89 -> 144 -> 233 -> 377 -> 610 -> 987 -> 1597 -> null", stringify(new Node(0, new Node(1, new Node(1, new Node(2, new Node(3, new Node(5, new Node(8, new Node(13, new Node(21, new Node(34, new Node(55, new Node(89, new Node(144, new Node(233, new Node(377, new Node(610, new Node(987, new Node(1597))))))))))))))))))));
+    }
+    
     public static String stringify(Node list) {
         List<String> result = new ArrayList<>();
         while (list != null){
