2410d243a81c84cf970de355a22dc382db801e06
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-08-15 22:28:32 +0530

    Added description and minor refactor to code of CamelCase

--- src/test/java/io/github/sandeeplakka/codewars/kyu6/CamelCase.java
+++ src/test/java/io/github/sandeeplakka/codewars/kyu6/CamelCase.java
@@ -9,6 +9,19 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+CamelCase Method
+
+Write simple camelCase for strings.
+All words must have their first letter capitalized without spaces.
+
+For instance:
+camelCase("hello case"); // => "HelloCase"
+camelCase("camel case word"); // => "CamelCaseWord"
+
+
+Categories : Fundamentals, Algorithms, Strings
+ */
 public class CamelCase {
 
     @Test
@@ -81,13 +94,15 @@ public class CamelCase {
 
     public static String camelCase(String str) {
         // your code here
-        return Arrays.stream(str.split(" ")).map(CamelCase::capitalize).collect(Collectors.joining());
+        return Arrays.stream(str.split(" "))
+                .map(CamelCase::capitalize)
+                .collect(Collectors.joining());
     }
 
     private static String capitalize(String input) {
         if (input.isEmpty()) return input;
         if (input.length() == 1) return input.toUpperCase();
-        return input.substring(0, 1).toUpperCase() + input.substring(1);
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
 }
