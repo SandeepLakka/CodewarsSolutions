700deaf6bc48367c410ec42eb2c4266415e0aa3d
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-08-31 08:31:07 +0530

    Added RgbToHex problem with impl and TCs

---
+++ src/test/java/io/github/sandeeplakka/codewars/kyu5/RgbToHex.java
@@ -0,0 +1,71 @@
+package io.github.sandeeplakka.codewars.kyu5;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+RGB To Hex Conversion
+
+The rgb function is incomplete. Complete it so that passing in RGB decimal values
+will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255.
+Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+RgbToHex.rgb(255, 255, 255) // returns FFFFFF
+RgbToHex.rgb(255, 255, 300) // returns FFFFFF
+RgbToHex.rgb(0, 0, 0)       // returns 000000
+RgbToHex.rgb(148, 0, 211)   // returns 9400D3
+
+Categories : Algorithms
+ */
+public class RgbToHex {
+
+    @Test
+    public void sampleTests() {
+        assertEquals("000000", RgbToHex.rgb(0, 0, 0), "For input: (0, 0, 0)");
+        assertEquals("010203", RgbToHex.rgb(1, 2, 3), "For input: (1, 2, 3)");
+        assertEquals("FFFFFF", RgbToHex.rgb(255, 255, 255), "For input: (255, 255, 255)");
+        assertEquals("FEFDFC", RgbToHex.rgb(254, 253, 252), "For input: (254, 253, 252)");
+        assertEquals("00FF7D", RgbToHex.rgb(-20, 275, 125), "For input: (-20, 275, 125)");
+    }
+
+    public static String rgb(int r, int g, int b) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(convertToHex(r));
+        builder.append(convertToHex(g));
+        builder.append(convertToHex(b));
+
+        return builder.toString();
+    }
+
+    private static String convertToHex(int decimal) {
+        Map<Integer, Character> hexCodes = new HashMap<>();
+        for (int i = 0; i < 16; i++) {
+            char ch;
+            if (i >= 10) {
+                ch = (char) ('A' + (i - 10));
+            } else {
+                ch = (char) ('0' + (i));
+            }
+            hexCodes.put(i, ch);
+        }
+
+        if (decimal <= 0) return "00";
+        if (decimal >= 255) return "FF";
+        StringBuilder builder = new StringBuilder();
+        int rem;
+        while (decimal > 0) {
+            rem = decimal % 16;
+            builder.append(hexCodes.get(rem));
+            decimal /= 16;
+        }
+        if (builder.length() == 1) builder.append("0");
+        return builder.reverse().toString();
+    }
+}
